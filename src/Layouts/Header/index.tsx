4cb1cbc6440ccf51047256bdfc478c1e10e8504b
RaCaTaSh <racatashsp@gmail.com>
2022-06-13 15:11:39 -0300

    Primer commit

---
+++ src/Layouts/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header =()=>{
+  return(
+    <nav className="has-background-info has-text-grey h-30px">
+      asd
+    </nav>
+  )
+}
+export default Header
---
+++ src/Layouts/Main/index.tsx
@@ -0,0 +1,12 @@
+
+
+import Logo from "../../Assets/Images/descarga.png";
+const Home = () => {
+return(
+  <div>
+    <h1>home</h1>
+    <img src={Logo} alt="" width={30}/>
+  </div>
+)
+}
+export default Home
